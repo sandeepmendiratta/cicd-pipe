29df66600cd4aabca47acdd242f979ca6fd2802f
Sandeep Mendiratta <smendiratta@yahoo.com>
2018-04-12 14:35:43 -0500

    fixes

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-# test
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-#docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-FILENAME="smendiratta/cicd-pipe"
-docker build -f Dockerfile -t $FILENAME:$TAG .
-docker push $FILENAME:$TAG
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG
